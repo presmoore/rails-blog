5de3f4705ba0835737bdfbf7584c7988771242da
Jeremy Raines <jraines@gmail.com>
2015-07-07 13:23:41 -0400

    Empty new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsblog::Application.config.secret_key_base = '9401d4b59913aa5129c7b39dd3b54efa44d7bc65c1b19761169274b59860d5ade8f6f55515a3f7f8afef1a4505f86cf6106bef7c33ce1bf7fa8350f4e8c10f9e'
