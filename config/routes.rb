f0606fec0415b415e9d45ce6b0a80b7c51e7922f
Preston Moore <prestonmoore@me.com>
2015-07-08 18:16:12 -0400

    started trying to implement comments on posts; not working yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Railsblog::Application.routes.draw do
 
+  resources :comments
+
   resources :posts
 
   root 'users#index' 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708195855) do
+ActiveRecord::Schema.define(version: 20150708214146) do
+
+  create_table "comments", force: true do |t|
+    t.string   "textarea"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
